0f2258d3829dee7c9c460a66b6b2d1ee6ef007b8
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2024-02-12 22:43:10 +0530

    Custom React develoupment

--- 01Basics/src/App.jsx
+++ 01Basics/src/App.jsx
@@ -1,12 +1,13 @@
 
 import Pv from "./pv"
+const name = "Pranav"
 function App() {
  
 
   return (
     <>
     <Pv/>
-    <h3>Hello World</h3>
+    <h3>Hello {name}</h3>  name is the evaluated expression
     
     </>
   )
